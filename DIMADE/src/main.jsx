aa4c67e13b96e2454e7722f76c8de851faf4b1e8
rodrigoperezmoya <rodrigo.perez.moya@gmail.com>
2025-05-19 23:35:56 -0400

    primer commit

--- DIMADE/src/main.jsx
+++ DIMADE/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline } from "@mui/material";
 import App from "./App";
+import Nosotros from "./vistas/vistas_front/Nosotros";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
     <App />
+    <Nosotros />
   </React.StrictMode>
 );
