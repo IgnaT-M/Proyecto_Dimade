1197d0659c970c156b981b7572d8b7bdd36dafc0
PabloIgna <wpablotm@gmail.com>
2025-07-16 01:34:49 -0400

    otro mas

--- DIMADE/src/config/apiConfig.js
+++ DIMADE/src/config/apiConfig.js
@@ -1,6 +1,4 @@
-
-//const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
-import.meta.env.VITE_API_URL || "https://proyecto-dimade.onrender.com";
-
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "https://proyecto-dimade.onrender.com";
 
 export default BASE_URL;
